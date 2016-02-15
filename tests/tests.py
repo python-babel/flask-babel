87a5fcfc54855cf9ad08fee99987de8cb4b89852
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-02-15 22:03:19 +0100

    Add passing test for custom locale

--- tests/tests.py
+++ tests/tests.py
@@ -74,30 +74,30 @@ class DateFormattingTestCase(unittest.TestCase):
         with app.test_request_context():
             assert format_datetime(d) == 'April 12, 2010 3:46:00 AM'
 
-    # def test_custom_locale_selector(self):
-    #     app = flask.Flask(__name__)
-    #     b = babel.Babel(app)
-    #     d = datetime(2010, 4, 12, 13, 46)
-    #
-    #     the_timezone = 'UTC'
-    #     the_locale = 'en_US'
-    #
-    #     @b.localeselector
-    #     def select_locale():
-    #         return the_locale
-    #     @b.timezoneselector
-    #     def select_timezone():
-    #         return the_timezone
-    #
-    #     with app.test_request_context():
-    #         assert babel.format_datetime(d) == 'Apr 12, 2010, 1:46:00 PM'
-    #
-    #     the_locale = 'de_DE'
-    #     the_timezone = 'Europe/Vienna'
-    #
-    #     with app.test_request_context():
-    #         assert babel.format_datetime(d) == '12.04.2010 15:46:00'
-    #
+    def test_custom_locale_selector(self):
+        app = flask.Flask(__name__)
+        icu = ICU(app)
+        d = datetime(2010, 4, 12, 13, 46)
+
+        the_timezone = 'UTC'
+        the_locale = 'en_US'
+
+        @icu.localeselector
+        def select_locale():
+            return the_locale
+        @icu.timezoneselector
+        def select_timezone():
+            return the_timezone
+
+        with app.test_request_context():
+            assert format_datetime(d) == 'Apr 12, 2010, 1:46:00 PM'
+
+        the_locale = 'de_DE'
+        the_timezone = 'Europe/Vienna'
+
+        with app.test_request_context():
+            assert format_datetime(d) == '12.04.2010 15:46:00'
+
     # def test_refreshing(self):
     #     app = flask.Flask(__name__)
     #     b = babel.Babel(app)
