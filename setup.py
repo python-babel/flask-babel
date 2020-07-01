b24f8372cd24b4385857716d3b8ad0bc9be356a9
Jürgen Gmach <juergen.gmach@googlemail.com>
2020-07-01 20:53:12 +0200

    Add support for Python 3.7 and Python 3.8
    modified:   .travis.yml
    modified:   setup.py
    modified:   tox.ini

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
