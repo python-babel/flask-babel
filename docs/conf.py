cb69124b4d5bc0de7f2541f770556568ffbd478a
Tyler Kennedy <tk@tkte.ch>
2023-01-15 21:03:11 -0500

    Bump version: 2.0.0 → 3.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Armin Ronacher'
 author = 'Armin Ronacher'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '3.0.0'
 
 
 # -- General configuration ---------------------------------------------------
