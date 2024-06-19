e63da291b4dc1a4647fde8154e1eb55120bb90b6
Tyler Kennedy <tk@tkte.ch>
2024-06-19 02:43:29 -0400

    Lint the entire repo

--- tests/test_multiple_apps.py
+++ tests/test_multiple_apps.py
@@ -6,17 +6,15 @@ def test_multiple_apps():
     b = babel.Babel()
 
     app1 = flask.Flask(__name__)
-    b.init_app(app1, default_locale='de_DE')
+    b.init_app(app1, default_locale="de_DE")
 
     app2 = flask.Flask(__name__)
-    b.init_app(app2, default_locale='en_US')
+    b.init_app(app2, default_locale="en_US")
 
     with app1.test_request_context():
-        assert str(babel.get_locale()) == 'de_DE'
-        assert babel.gettext(u'Hello %(name)s!', name='Peter') == \
-               'Hallo Peter!'
+        assert str(babel.get_locale()) == "de_DE"
+        assert babel.gettext("Hello %(name)s!", name="Peter") == "Hallo Peter!"
 
     with app2.test_request_context():
-        assert str(babel.get_locale()) == 'en_US'
-        assert babel.gettext(u'Hello %(name)s!', name='Peter') == \
-               'Hello Peter!'
+        assert str(babel.get_locale()) == "en_US"
+        assert babel.gettext("Hello %(name)s!", name="Peter") == "Hello Peter!"
